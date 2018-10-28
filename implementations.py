3daa6f9f120b5268dc13b259b9e758b8b72e2b41
TALBI OUIAME <ouiame.talbi@epfl.ch>
2018-10-28 14:36:12 +0100

    GD and SGD methods

--- implementations.py
+++ implementations.py
@@ -18,3 +18,46 @@ def compute_mse(y, tx, w):
     error = y - np.dot(tx, w)
     mse = np.dot(np.transpose(error), error)/(2 * len(y))
     return mse
+
+def compute_gradient(y, tx, w):
+    """Compute the gradient."""
+    err = y - tx.dot(w)
+    return -tx.T.dot(err) / len(err)
+
+def compute_loss(y, tx, w):
+    """Calculate the loss using mse"""
+    e = y - tx.dot(w)
+    return calculate_mse(e)
+
+def gradient_descent(y, tx, initial_w, max_iters, gamma):
+    """Gradient descent algorithm."""
+    w = initial_w
+    loss = 0
+    for n_iter in range(max_iters):
+        # compute loss, gradient
+        grad = compute_gradient(y, tx, w)
+        loss = compute_loss(y,tx,w)
+        # gradient w by descent update
+        w = w - gamma * grad
+
+    return loss, w
+
+
+def compute_stoch_gradient(y, tx, w):
+    """Compute a stochastic gradient from just few examples n and their corresponding y_n labels."""
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
+
+
+def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+    w = initial_w
+    for n_iter in range(max_iters):
+        for y_batch, tx_batch in batch_iter(y, tx,batch_size, 1):
+            # compute a stochastic gradient and loss
+            grad, _ = compute_stoch_gradient(y_batch, tx_batch, w)
+            # update w through the stochastic gradient update
+            w = w - gamma * grad
+            # calculate loss
+            loss = compute_loss(y, tx, w)
+    return loss, w
