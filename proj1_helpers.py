440c32351d8c2996f35285aadbe92863e7c7a832
bharathn1990 <43513576+bharathn1990@users.noreply.github.com>
2018-10-29 17:14:48 +0100

    Add files via upload
    Final except run.py which sandra has.

--- proj1_helpers.py
+++ proj1_helpers.py
@@ -2,9 +2,10 @@
 """some helper functions for project 1."""
 import csv
 import numpy as np
-from logistic_regression import * 
+from implementations import * 
 from collections import Counter
 
+# Split data into training and test
 def split_data(tx, y, ratio, seed=1):
     """
     split the dataset based on the split ratio. If ratio is 0.8 
@@ -21,84 +22,62 @@ def split_data(tx, y, ratio, seed=1):
     test_y = y[ind[limit:]]
     return train_tx, train_y, test_tx, test_y
 
-# Split the data by jet number, with 2 and 3 grouped as one
-def split_data_by_jet_num(tx, y, ids):
-
-    tx_jet_num_0 = tx[tx[:, 22] == 0]
-    tx_jet_num_1 = tx[tx[:, 22] == 1]
-    tx_jet_num_others = tx[tx[:, 22] > 1]
-    y_jet_num_0 = y[tx[:, 22] == 0]
-    y_jet_num_1 = y[tx[:, 22] == 1]
-    y_jet_num_others = y[tx[:, 22] > 1]
-    ids_jet_num_0 = ids[tx[:, 22] == 0]
-    ids_jet_num_1 = ids[tx[:, 22] == 1]
-    ids_jet_num_others = ids[tx[:, 22] > 1]
-
-    tx_jet_num_0 = np.delete(tx_jet_num_0, [22, 4, 5, 6, 12, 23, 24, 25, 26, 27, 28, 29], axis=1)
-    tx_jet_num_1 = np.delete(tx_jet_num_1, [22, 4, 5, 6, 12, 26, 27, 28], axis=1)
-    tx_jet_num_others = np.delete(tx_jet_num_others, [22], axis=1)
-
-    return tx_jet_num_0, y_jet_num_0, ids_jet_num_0, tx_jet_num_1, y_jet_num_1, ids_jet_num_1, \
-     tx_jet_num_others, y_jet_num_others, ids_jet_num_others
-
-def split_data_by_jet_num_1(tx, y, ids):
-		
-    tx_jet_num =  [  tx[tx[:, 22] == 0],  tx[tx[:, 22] == 1],  tx[tx[:, 22] >= 2]]
-    
-    y_jet_num =   [   y[tx[:, 22] == 0],   y[tx[:, 22] == 1],   y[tx[:, 22] >= 2]] 
-    
-    ids_jet_num = [ ids[tx[:, 22] == 0], ids[tx[:, 22] == 1], ids[tx[:, 22] >= 2]]
-    	
-    # tx_jet_num[0] = np.delete(tx_jet_num[0], [22, 8, 4, 5, 6, 12, 23, 24, 25, 26, 27, 28, 29], axis=1)
-    # tx_jet_num[1] = np.delete(tx_jet_num[1], [22, 4, 5, 6, 12, 26, 27, 28, 29], axis=1)
-    # tx_jet_num[2] = np.delete(tx_jet_num[2], [22, 29], axis=1)
-    tx_jet_num[2] = np.delete(tx_jet_num[2], [22, 15, 18, 20, 25,28, 29], axis=1)
-    # tx_jet_num[3] = np.delete(tx_jet_num[3], [22, 29], axis=1)
-
-	#Removing some PHI as well
-    tx_jet_num[0] = np.delete(tx_jet_num[0], [22, 8, 4, 5, 6, 12, 15, 18, 20, 23, 24, 25, 26, 27, 28, 29], axis=1)
-    tx_jet_num[1] = np.delete(tx_jet_num[1], [22, 4, 5, 6, 12, 15, 18, 20, 25, 26, 27, 28, 29], axis=1)
-    # tx_jet_num[2] = np.delete(tx_jet_num[2], [22, 15, 18, 20, 25, 27, 28, 29], axis=1)
-    # tx_jet_num[3] = np.delete(tx_jet_num[3], [22, 15, 18, 20, 25, 27,  29], axis=1)
+# Split data by jet number after adding the 4 extra features 	
+def split_data_by_jet_num_feature(tx, y, ids):
+    '''We first add 4 additional features:
+    1. Assymetric Energy
+    2. delta phi
+    3. Avg_phi
+    4. A special variable which is (DER_mass_MMC * DER_pt_ratio) / DER_sum_pt
+    We then split the existing tx into 2 - one set with valid DER_mass_MMC and the other with invalid DER_mass_MMC.
+    Next, we split the two tx sets into the 4 jet numbers.
+    Finally, we remove the columns with -999 (these are the values that are not calculated for the current jet) and other 
+    variables deemed unnecessary using the histogram plots.
+    '''	
+    n_rows = tx.shape[0]
 	
-	#Removing only PHI
-    # tx_jet_num[0] = np.delete(tx_jet_num[0], [22, 15, 18, 20, 25, 27, 28], axis=1)
-    # tx_jet_num[1] = np.delete(tx_jet_num[1], [22, 15, 18, 20, 25, 27, 28], axis=1)
-    # tx_jet_num[2] = np.delete(tx_jet_num[2], [22, 15, 18, 20, 25, 27, 28], axis=1)
-    # tx_jet_num[3] = np.delete(tx_jet_num[3], [22, 15, 18, 20, 25, 27, 28], axis=1)
-
+	# Calculate additional features reported in paper 
+    eMHT = tx[:,29] - tx[:,26] - tx[:,23] # Needed for assymetric energy below
+    assym = np.divide(tx[:,19] - eMHT, tx[:,19] + eMHT)
+    delta_phi = tx[:,25] - tx[:,28]
+    delta_phi_met = 0.5*(tx[:,25] + tx[:,28])
+    special = np.divide( np.multiply(tx[:,0],tx[:,10]) , tx[:,9] )
 	
-    return tx_jet_num, y_jet_num, ids_jet_num	 	 
-	 
-	 
-# Split the data by jet number, with 2 and 3 separate	 
-def split_data_by_jet_num_2(tx, y, ids):
-		
-    tx_jet_num =  [  tx[tx[:, 22] == 0],  tx[tx[:, 22] == 1],  tx[tx[:, 22] == 2],  tx[tx[:, 22] == 3]]
+	# Add the features to tx
+    tx = np.append(tx,np.reshape(assym,(n_rows,1)),axis= 1)
+    tx = np.append(tx,np.reshape(delta_phi,(n_rows,1)),axis= 1)
+    tx = np.append(tx,np.reshape(delta_phi_met,(n_rows,1)),axis= 1)
+    tx = np.append(tx,np.reshape(special,(n_rows,1)),axis= 1)
+	
+	# Split the tx data into those rows with DER_mass_MMC = -999 and those having valid DER_mass_MMC
+    tx_mass 	= tx[tx[:,0] > -999]
+    tx_no_mass 	= tx[tx[:,0] == -999]
+    y_mass = y[tx[:,0] > -999]
+    y_no_mass = y[tx[:,0]== -999]
+    ids_mass = ids[tx[:,0] > -999]
+    ids_no_mass = ids[tx[:,0] == -999]
+	
+	# Split the 2 datasets by jet numbers and create a list of tx, y and ids for the jet numbers
+    tx_jet_num =  [ tx_mass[tx_mass[:, 22] == 0], tx_mass[tx_mass[:, 22] == 1], tx_mass[tx_mass[:, 22] == 2], tx_mass[tx_mass[:, 22] == 3], 
+	tx_no_mass[tx_no_mass[:, 22] == 0],  tx_no_mass[tx_no_mass[:, 22] == 1],  tx_no_mass[tx_no_mass[:, 22] == 2],  tx_no_mass[tx_no_mass[:, 22] == 3]]
     
-    y_jet_num =   [   y[tx[:, 22] == 0],   y[tx[:, 22] == 1],   y[tx[:, 22] == 2],   y[tx[:, 22] == 3]] 
-    
-    ids_jet_num = [ ids[tx[:, 22] == 0], ids[tx[:, 22] == 1], ids[tx[:, 22] == 2], ids[tx[:, 22] == 3] ]
-    	
-    # tx_jet_num[0] = np.delete(tx_jet_num[0], [22, 8, 4, 5, 6, 12, 23, 24, 25, 26, 27, 28, 29], axis=1)
-    # tx_jet_num[1] = np.delete(tx_jet_num[1], [22, 4, 5, 6, 12, 26, 27, 28, 29], axis=1)
-    # tx_jet_num[2] = np.delete(tx_jet_num[2], [22, 29], axis=1)
-    tx_jet_num[2] = np.delete(tx_jet_num[2], [22, 15, 18, 20, 25,28, 29], axis=1)
-    # tx_jet_num[3] = np.delete(tx_jet_num[3], [22, 29], axis=1)
-
-	#Removing some PHI as well
-    tx_jet_num[0] = np.delete(tx_jet_num[0], [22, 8, 4, 5, 6, 12, 15, 18, 20, 23, 24, 25, 26, 27, 28, 29], axis=1)
-    tx_jet_num[1] = np.delete(tx_jet_num[1], [22, 4, 5, 6, 12, 15, 18, 20, 25, 26, 27, 28, 29], axis=1)
-    # tx_jet_num[2] = np.delete(tx_jet_num[2], [22, 15, 18, 20, 25, 27, 28, 29], axis=1)
-    tx_jet_num[3] = np.delete(tx_jet_num[3], [22, 15, 18, 20, 25, 27,  29], axis=1)
+    y_jet_num = [y_mass[tx_mass[:, 22] == 0],   y_mass[tx_mass[:, 22] == 1],   y_mass[tx_mass[:, 22] == 2],   y_mass[tx_mass[:, 22] == 3], 
+	y_no_mass[tx_no_mass[:, 22] == 0],   y_no_mass[tx_no_mass[:, 22] == 1],   y_no_mass[tx_no_mass[:, 22] == 2],   y_no_mass[tx_no_mass[:, 22] == 3]] 
 	
-	#Removing only PHI
-    # tx_jet_num[0] = np.delete(tx_jet_num[0], [22, 15, 18, 20, 25, 27, 28], axis=1)
-    # tx_jet_num[1] = np.delete(tx_jet_num[1], [22, 15, 18, 20, 25, 27, 28], axis=1)
-    # tx_jet_num[2] = np.delete(tx_jet_num[2], [22, 15, 18, 20, 25, 27, 28], axis=1)
-    # tx_jet_num[3] = np.delete(tx_jet_num[3], [22, 15, 18, 20, 25, 27, 28], axis=1)
-
+    ids_jet_num = [ids_mass[tx_mass[:, 22] == 0],   ids_mass[tx_mass[:, 22] == 1],   ids_mass[tx_mass[:, 22] == 2],   ids_mass[tx_mass[:, 22] == 3], 
+	ids_no_mass[tx_no_mass[:, 22] == 0],   ids_no_mass[tx_no_mass[:, 22] == 1],   ids_no_mass[tx_no_mass[:, 22] == 2],   ids_no_mass[tx_no_mass[:, 22] == 3]] 
 	
+	# Remove -999 features and features which are deemed unnecessary (like the PHI features)
+    tx_jet_num[0] = np.delete(tx_jet_num[0], [   4, 5, 6, 8, 12, 15, 18, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32], axis=1) # Jet 0, valid DER_MASS_MMC
+    tx_jet_num[1] = np.delete(tx_jet_num[1], [   4, 5, 6,    12, 15, 18, 20, 22,         25, 26, 27, 28, 29,     31, 32], axis=1) # Jet 1, valid DER_MASS_MMC
+    tx_jet_num[2] = np.delete(tx_jet_num[2], [                   15, 18, 20, 22,         25,         28                ], axis=1) # Jet 2, valid DER_MASS_MMC
+    tx_jet_num[3] = np.delete(tx_jet_num[3], [                   15, 18, 20, 22,         25,     27,     29            ], axis=1) # Jet 3, valid DER_MASS_MMC
+	    
+    tx_jet_num[4] = np.delete(tx_jet_num[4], [0, 4, 5, 6, 8, 12, 15, 18, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32], axis=1) # Jet 0, invalid DER_MASS_MMC    
+    tx_jet_num[5] = np.delete(tx_jet_num[5], [0, 4, 5, 6,    12, 15, 18, 20, 22,         25, 26, 27, 28, 29,     31, 32], axis=1) # Jet 1, invalid DER_MASS_MMC
+    tx_jet_num[6] = np.delete(tx_jet_num[6], [0,                 15, 18, 20, 22,         25,         28                ], axis=1) # Jet 2, invalid DER_MASS_MMC
+    tx_jet_num[7] = np.delete(tx_jet_num[7], [0,                 15, 18, 20, 22,         25,     27,     29            ], axis=1) # Jet 3, invalid DER_MASS_MMC
+	    
     return tx_jet_num, y_jet_num, ids_jet_num	 
 	 
 def read_csv_headers(data_path):
@@ -127,8 +106,9 @@ def load_csv_data(data_path, sub_sample=False):
 
     return yb, input_data, ids
 
-#Create polynomial terms for each column of tx 
+ 
 def get_polynomial(tx,poly_term):
+	'''Create polynomial terms for each column of tx'''
 	tx_temp = tx.copy()
 	tx_return = tx.copy()
 	for i in range(poly_term-1):
@@ -143,151 +123,70 @@ def get_data_by_type(input_data, type_tag):
     elif type_tag == 'DER':
         input_data_ss = input_data[:, 13:]
     return input_data_ss
-
-# Standardize the training data using Z score standardization
-def standardize_training(x):
-	mean_x = np.mean(x,axis=0)
-	x = x - mean_x
-	std_x = np.std(x,axis=0)
-	x = x / std_x
-	return x, mean_x, std_x
 	
-# Standardize the test data using the mean and SD of the training data
+
 def standardize_test(x,mean_x,std_x):
-	"""Standardize the original data set."""
+	'''Standardize the test data using the mean and SD of the training data'''
 	x = x - mean_x
 	x = x / std_x
 	return x
 
-# Standardize data using min-max standardization
-# NOT REQUIRED
-def standardize_minmax(tx):
-	n_col = tx.shape[1]
-	tx_return = tx.copy()
-	for col in range(n_col):
-		tx_return[:,col] = (tx[:,col] - min(tx[:,col]))/(max(tx[:,col]) - min(tx[:,col]))
-	return tx_return
 
-# Remove outliers from the dataset. i.e. points which are more than 2 SD away.
 def remove_outliers(tx,y,threshold):
-    #Author : BN 
-	#Date: 17/10/2018
-    shape_tx = np.shape(tx)
-    n_columns = shape_tx[1]
-    for i_col in range(n_columns):
-        y = y[np.where(np.abs(tx[:,i_col]) < threshold)]
-        tx = tx[np.where(np.abs(tx[:,i_col]) < threshold)]
-        y_out = y[np.where(np.abs(tx[:,i_col]) >= threshold)]
-        tx_out = tx[np.where(np.abs(tx[:,i_col]) >= threshold)]       
-    return tx, y, tx_out, y_out
+	'''Remove outliers from the dataset. i.e. points which are more than a certain SD away.'''
+	shape_tx = np.shape(tx)
+	n_columns = shape_tx[1]
+	for i_col in range(n_columns):
+		y = y[np.where(np.abs(tx[:,i_col]) < threshold)]
+		tx = tx[np.where(np.abs(tx[:,i_col]) < threshold)]
+	return tx, y
 
-# In the test data, set the prediction for all samples with -999 in DER_MASS to background
-def set_background(tx,y, sd):
-	# der_mass_mmc = tx[:,0] # Get the der_mass_mmc column from tx
-	# print('y before setting =', y[np.where(der_mass_mmc == -999)])
-	# y[np.where(der_mass_mmc == -999)] = -1
-	# print('y after setting =', y[np.where(der_mass_mmc == -999)])
-	count = 0
-	# Set background for outliers
-	n_rows = tx.shape[0]
-	for i in range(n_rows):
-		row_vec = tx[i,:]
-		n_outliers = np.where(row_vec > sd)
-		# print(np.shape(n_outliers)[1])
-		if((np.shape(n_outliers)[1]) > 5):
-			count +=1
-			y[i] = -1
-		else:
-			n_outliers = np.where(row_vec < -1*sd)
-			if((np.shape(n_outliers)[1]) > 5):
-				count +=1
-				y[i] = -1
-		
-	print('number changed =', count)
-	
-	return y
 
-def redo_standardization(tx, mean, sd):
-	tx = tx*sd
-	tx = tx + mean
-	tx, mean_tx,std_tx = standardize_training(tx)
-	return tx, mean_tx, std_tx
+def standardize_training(tx, y, threshold):
+	''' Remove outliers and standardize the data'''
+	# Perform an initial standardization of the data 
+	mean_tx = np.mean(tx,axis=0)
+	tx = tx - mean_tx
+	std_tx = np.std(tx,axis=0)
+	tx = tx / std_tx
 	
-# Replace -999 in a column with mean of remaining elements
-def replace_999(tx):
-	n_col = tx.shape[1]
-	tx_return = tx.copy()
+	# Remove the outliers using this standardized data
+	tx, y = remove_outliers(tx,y,threshold)
 	
-	for col in range(n_col):
-		vec_temp = tx[:,col]
-		vec = vec_temp[vec_temp > -999]
-		if(len(vec) > 0):
-			# mean_vec = np.mean(vec)
-			mean_vec = np.median(vec)
-			# vec_temp[vec_temp == -999] = mean_vec
-			vec_temp[vec_temp == -999] = mean_vec
-		tx_return[:,col] = vec_temp
-	return tx_return
+	# Recover original values of the cleaned dataset using the std and mean calculated earlier
+	tx = tx*std_tx
+	tx = tx + mean_tx
+	
+	# Standardize the cleaned dataset to obtain the correct mean and std
+	mean_tx = np.mean(tx,axis=0)
+	tx = tx - mean_tx
+	std_tx = np.std(tx,axis=0)
+	tx = tx / std_tx
+	
+	return tx, y, mean_tx, std_tx
 
-# Replace -999 in der_mass_mmc with the mean of the background
-def replace_999_mass(tx,y):
-	der_mass_mmc = tx[:,0] # Get the der_mass_mmc column from tx
-	vec_bg = der_mass_mmc[np.where(y == -1)] # vector containing mass entries for the background only
-	# print('No. of background = ', np.shape(vec_bg)) 
-	vec_bg = vec_bg[vec_bg > -999]  # get non -999 entries to calculate their mean and median
-	# print('No. of valid background = ', np.shape(vec_bg))
-	mean_bg = np.mean(vec_bg)
-	# print('Mean. of valid background = ', mean_bg)
-	median_bg = np.median(vec_bg)
-	
-	# Replace all -999 in der_mass_mmc with the median or mean of the background data with proper values
-	# print('der_mass BEFORE', tx[:,0])
-	der_mass_mmc[np.where(tx[:,0] == -999)] = median_bg
-	
-	# difference_vector = der_mass_mmc - tx[:,0]
-	# print('difference vector = ', difference_vector)
-	
-	tx[:,0] = der_mass_mmc
-	# print('der_mass AFTER', tx[:,0])
-	return tx
-	
-	
-#remove columns with large 999
-def remove_columns_invalid(input_data, thresh):
-    input_data_ss = input_data
-    ct = 0
-    for column in input_data.T:
-        num_invalid = len(column[np.where(column == -999)])
-        # print(float(num_invalid) / len(column) * 100)
-        if float(num_invalid) / len(column) * 100 > thresh:
-            input_data_ss = np.delete(input_data_ss, ct, 1)
-            # print("Shape:", np.shape(input_data_ss))
-            # print('ct is:',ct)            
-#         ct += 1
-        else: ct += 1 # BN edit to account for renumbering of columns
-    return input_data_ss
 
-#Generates class predictions given weights, and a test data matrix
-def predict_labels(weights, data):
-    
-    # y_pred = np.dot(data, weights)
-    y_pred = sigmoid(np.dot(data, weights))
-	# sigm = sigmoid(y_pred)
-    # print(y_pred)
-    # print(sigm[sigm <= 0.5])
-    y_pred[np.where(y_pred < 0.5)] = -1
-    y_pred[np.where(y_pred >= 0.5)] = 1
-    
-    return y_pred
+def predict_labels(weights, data, isLogistic):
+    '''Generates class predictions given weights, and a test data matrix'''
+	# If the classification used is logistic regression
+	if(isLogistic):
+		y_pred = sigmoid(np.dot(data, weights)) # Obtain the log function values
+		y_pred[np.where(y_pred < 0.5)] = -1
+		y_pred[np.where(y_pred >= 0.5)] = 1
+	
+	else: # If we use linear regression
+		y_pred = np.dot(data, weights)
+		y_pred[np.where(y_pred < 0)] = -1
+		y_pred[np.where(y_pred >= 0)] = 1
+	return y_pred
 
-#Verify the predicted y against the test y for cross validation data sets 
+
 def verify_prediction(y_pred, y_test):
-    y_diff = y_pred - y_test
-    # print(len(y_diff))
-    nFalse = len(y_diff[y_diff !=0 ])
-    # accuracy = 1 - np.sum(y_diff)/len(y_diff)
-    accuracy = 1 - nFalse/len(y_diff)
-    return accuracy
+	'''Verify the predicted y against the test y for cross validation data sets '''
+	y_diff = y_pred - y_test 		  # difference between the predicted and actual test values
+	nFalse = len(y_diff[y_diff !=0 ]) # Number of elements whose difference is non-zero
+	accuracy = 1 - nFalse/len(y_diff)
+	return accuracy
 	
 def create_csv_submission(ids, y_pred, name):
     """
---
+++ run_cross_validation.py
@@ -0,0 +1,107 @@
+from proj1_helpers_1 import *
+from implementations import *
+
+def run_model(train_ratio, stepsize, lambda_):
+
+	training_data_path = "train.csv"
+	test_data_path = "test.csv"
+	output_path = "output.csv"
+	n_trials = 10
+	limit = int(train_ratio * 250000)
+	sd_limit_0, sd_limit_1, sd_limit_2, sd_limit_3 = 3.0, 2.75, 2.75, 2.6
+	list_sd_limit = [sd_limit_0,sd_limit_1,sd_limit_2,sd_limit_3, sd_limit_0,sd_limit_1,sd_limit_2,sd_limit_3]
+	list_poly = [2,2,2,2,2,2,2,2] # List of polynomials  for jets 0 to 3, for DER_mass_MMC valid and invalid
+	weights_average = np.array([0, 0 ,0 ,0, 0, 0 ,0 ,0]) # Average weights 
+	max_iters = 10000
+	
+	print('stepsize = ', stepsize, '\t', 'lambda = ', lambda_)
+	
+	#Read training data	
+	training_y_full, training_tx_full, training_ids = load_csv_data(training_data_path, sub_sample=False)
+	accuracy_average = 0
+	
+	for i_trial in range(n_trials):
+		
+		#Split the data into test and training
+		training_tx, training_y, test_tx, test_y = split_data(training_tx_full, training_y_full, train_ratio, i_trial)
+					
+		# Split the training data by jet numbers and validity of DER_mass_MMC
+		list_training_tx, list_training_y, list_training_ids = split_data_by_jet_num_feature(training_tx, training_y, training_ids[:limit])
+					
+		# Split the test data by jet numbers and validity of DER_mass_MMC
+		list_test_tx, list_test_y, list_test_ids = split_data_by_jet_num_feature(test_tx, test_y, training_ids[:250000-limit])
+		
+		# List that will contain all the norms of the weights for each jet
+		list_weight_norms = np.array([0,0,0,0,0, 0 ,0 ,0])
+		
+		# Loop through jet numbers
+		for i in range(8):
+			
+			training_tx_i, training_y_i, training_ids_i = list_training_tx[i], list_training_y[i], list_training_ids[i]
+			test_tx_i, test_y_i, test_ids_i = list_test_tx[i], list_test_y[i], list_test_ids[i]
+			sd_limit = list_sd_limit[i]
+			
+			# ******************************************* TRAINING *************************************************************		
+			#Standardize the training data
+			training_tx_i, training_tx_i_mean, training_tx_i_std = standardize_training(training_tx_i)
+					
+			#Remove outliers from the standardized training dataset
+			training_tx_i, training_y_i, training_tx_i_out, training_y_i_out = remove_outliers(training_tx_i,training_y_i,sd_limit)
+
+			#Redo standardization
+			training_tx_i, training_tx_i_mean, training_tx_i_std = redo_standardization(training_tx_i, training_tx_i_mean, training_tx_i_std)
+			
+			#Create polynomial expansions
+			training_tx_i = get_polynomial(training_tx_i, list_poly[i])
+		
+			#Get weights
+			initial_w = np.zeros((training_tx_i.shape[1], ))
+			weights_i, loss_i = reg_logistic_regression_newton(training_y_i, training_tx_i,lambda_, initial_w, max_iters, stepsize)
+			
+			# ******************************************* PREDICTION *************************************************************
+			#Standardize the test data using training mean and std
+			test_tx_i_standardized = standardize_test(test_tx_i,training_tx_i_mean, training_tx_i_std)
+		
+			#Create polynomial expansion for the test data
+			test_tx_i_standardized = get_polynomial(test_tx_i_standardized, list_poly[i])
+			
+			#Get predictions and accuracy for the current jet
+			y_pred_i = predict_labels(weights_i, test_tx_i_standardized, 1) # 1 for logistic regression
+			accuracy = verify_prediction(y_pred_i, test_y_i)
+			
+			#Collate data for all jets
+			if(i == 0):
+				y_pred_all = y_pred_i
+				test_y_all = test_y_i
+				test_ids_all = test_ids_i
+				
+			else:
+				y_pred_all = np.concatenate((y_pred_all,y_pred_i))
+				test_y_all = np.concatenate((test_y_all,test_y_i))
+				test_ids_all = np.concatenate((test_ids_all,test_ids_i))
+				
+			list_weight_norms[i] = np.linalg.norm(weights_i)
+		
+		# Calculate average accuracy for the current test-train split using all jets
+		accuracy = verify_prediction(y_pred_all, test_y_all)
+		print(accuracy)
+		
+		# Add the current accuracy and weight norms to the average which will be outputted later
+		accuracy_average = ( accuracy + (accuracy_average*i_trial) )/(i_trial+1)
+		weights_average = ( list_weight_norms + (weights_average*i_trial) )/(i_trial+1)
+	
+	# Print the average accuracy and weight norm for the current test-train split after 10 fold cross validation
+	print('Accuracy Average is:', accuracy_average)
+	print('Weights Average Norms are:', weights_average)
+		
+
+if __name__ == "__main__":
+    train_ratio = 0.3
+    lambdas = 1.
+    gammas = [0.1]
+    	
+    for gamma in gammas:
+        run_model(train_ratio,gamma,lambdas)
+	
+
+	
